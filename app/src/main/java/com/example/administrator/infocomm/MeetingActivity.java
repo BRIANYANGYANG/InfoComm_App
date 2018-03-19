76feb8d97c13bb99e85cadbb82324feb3f38b638
yangpengfei <yang1029476796@163.com>
2018-03-19 16:57:19 +0800

    cm  会议模块ui完成 上传资源图片

--- app/src/main/java/com/example/administrator/infocomm/MeetingActivity.java
+++ app/src/main/java/com/example/administrator/infocomm/MeetingActivity.java
@@ -3,9 +3,7 @@ package com.example.administrator.infocomm;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
@@ -19,29 +17,10 @@ import butterknife.OnClick;
 
 public class MeetingActivity extends AppCompatActivity {
 
-    @BindView(R.id.title_text)
-    TextView title;
+
 
     @BindView(R.id.meeting_img)
     ImageView meetingImg;
-
-    @BindView(R.id.button1)
-    Button button1;
-
-    @BindView(R.id.button2)
-    Button button2;
-
-    @BindView(R.id.button3)
-    Button button3;
-
-    @BindView(R.id.button4)
-    Button button4;
-
-    @BindView(R.id.button5)
-    Button button5;
-
-    @BindView(R.id.button6)
-    Button button6;
 
     @OnClick(R.id.button1)
      void Button1() {
@@ -69,6 +48,16 @@ public class MeetingActivity extends AppCompatActivity {
         Glide.with(this).load(R.drawable.metting6).into(meetingImg);
     }
 
+    @OnClick(R.id.button7)
+    void Button7() {
+        Glide.with(this).load(R.drawable.metting7).into(meetingImg);
+    }
+
+    @OnClick(R.id.button8)
+    void Button8() {
+        Glide.with(this).load(R.drawable.metting8).into(meetingImg);
+    }
+
 
 
 
@@ -82,7 +71,6 @@ public class MeetingActivity extends AppCompatActivity {
         setContentView(R.layout.meeting_main);
         ButterKnife.bind(this);
 
-        title.setText("高峰会议");
 
     }
 }
