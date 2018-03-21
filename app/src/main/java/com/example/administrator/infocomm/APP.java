e460f61f63deb696eb2530cc9621115efedd7613
yangpengfei <yang1029476796@163.com>
2018-03-21 14:28:34 +0800

    cm  增加崩溃日志捕获

--- app/src/main/java/com/example/administrator/infocomm/APP.java
+++ app/src/main/java/com/example/administrator/infocomm/APP.java
@@ -2,8 +2,7 @@ package com.example.administrator.infocomm;
 
 import android.app.Application;
 
-
-
+import com.dreamlive.cn.clog.CollectLog;
 
 
 /**
@@ -16,6 +15,11 @@ public class APP  extends Application{
     public void onCreate() {
         super.onCreate();
 
+
+        //自定义日志存放路径,
+        //这儿示例就只传了sd根目录下的dreamlivemeng(/storage/emulated/0/dreamlivemeng)，把错误日志写到这个目录下
+        CollectLog clog = CollectLog.getInstance();
+        clog.init(this, "/sdcard/infocomm/log" );
     }
 
 
